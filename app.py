cc444a67b86fb449ac7d4c0daaf382da35512a28
Grikol <97914594+Grikol@users.noreply.github.com>
2022-01-30 14:03:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     
-    
+    text = "Siema G tu Ildefons koduje"   # open('xd.txt').read()
+
     text = open('xd.txt').read()
     
     return render_template("index.html", text=text)
